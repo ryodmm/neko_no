8652b4a9a65c5381ae3441dbe9ccf73b0322a5a3
Your Name <100946724+ryodmm@users.noreply.github.com>
2022-09-08 05:31:28 +0000

    edit_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,19 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'users/show'
-    get 'users/index'
-  end
-  namespace :admin do
-    get 'posts/show'
-    get 'posts/search'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-  end
-  namespace :public do
-    get 'posts/new'
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/edit'
-    get 'posts/search'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
+
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :users, :posts
+  end
+
+  scope module: :public do
+    resources :users
+
+    resources :posts
+    get 'posts/search' => 'posts#search'
+
+    root to: 'homes#top'
+  end
 end
