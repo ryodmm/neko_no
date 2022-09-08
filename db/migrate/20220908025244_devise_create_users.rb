2a541b8e7b512f216c73d1e84a817c44a8f55708
Your Name <100946724+ryodmm@users.noreply.github.com>
2022-09-08 06:19:35 +0000

    add_kaminari,bootstrap,fontawesome

--- db/migrate/20220908025244_devise_create_users.rb
+++ db/migrate/20220908025244_devise_create_users.rb
@@ -4,8 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false
+      t.text :introduction
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.boolean :is_active, null: false, default: true
 
       ## Recoverable
       t.string   :reset_password_token
---
+++ db/migrate/20220908053918_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+
+      t.integer :user_id
+      t.integer :favorite_id
+      t.text :introduction
+
+      t.timestamps
+    end
+  end
+end
