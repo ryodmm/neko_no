1754673116b449b8f2cbcd0475cef0c685a1774b
ryodmm <sora.okaomakkuro@gmail.com>
2022-10-03 09:37:59 +0000

    after_seiion_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,22 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def after_sign_in_path_for(resource)
-        posts_path(current_user)
+    def after_sign_in_path_for(resource_or_scope)
+      if resource_or_scope.is_a?(Admin)
+          admin_users_path
+      else
+          posts_path
+      end
     end
-    def after_sign_out_path_for(resource)
-        root_path
+
+    def after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :user
+          root_path
+      elsif resource_or_scope == :admin
+          new_admin_session_path
+      else
+          root_path
+      end
     end
 
   def configure_permitted_parameters
