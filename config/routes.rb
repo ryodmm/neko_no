9aef036565076fc4e84b3845409a5e12db7c45a0
ryodmm <sora.okaomakkuro@gmail.com>
2022-10-12 05:13:55 +0000

    edit_loginpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :admin, controllers: {
-  sessions: "admin/sessions"
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
   }
 
-  devise_for :users, controllers: {
+  devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
