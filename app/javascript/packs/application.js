b9175b48faaa67e57fb09771f64d70336d829b21
ryodmm <sora.okaomakkuro@gmail.com>
2022-12-20 11:37:33 +0000

    start_add_slideshow

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,10 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import 'jquery'
+import "script.js"
+import "skippr.min";
 import "channels"
-
-import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,6 +8,7 @@ environment.plugins.prepend(
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
+    "window.jQuery": "jquery",
     Popper: 'popper.js'
   })
 )
