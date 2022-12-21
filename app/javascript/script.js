d4e259eb191480600c72336d26283524d3f07dff
ryodmm <sora.okaomakkuro@gmail.com>
2022-12-21 08:42:04 +0000

    finish_side_show

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     // スライドショーの変化 ("fade" or "slide")
     transition : 'fade',
     // 変化に係る時間(ミリ秒)
-    speed : 1000,
+    speed : 1500,
     // easingの種類
     easing : 'easeOutQuart',
     // ナビゲーションの形("block" or "bubble")
@@ -15,7 +15,7 @@ $(document).ready(function () {
     // スライドショーの自動再生(falseで自動再生なし)
     autoPlay : true,
     // 自動再生時のスライド切替間隔(ミリ秒)
-    autoPlayDuration : 4000,
+    autoPlayDuration : 5000,
     // キーボードの矢印キーによるスライド送りの設定(trueで有効)
     keyboardOnAlways : false,
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
